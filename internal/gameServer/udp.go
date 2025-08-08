79f145d8417448ffd9c1b02ca2a2be0671af75a8
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-08-08 07:10:00 +0200

    Revert "Fix situation where target is 1 and health is 0 (#134)"
    This reverts commit 0a99655d8783ac6dfa86fce027d606d6c7ad0234.

--- internal/gameServer/udp.go
+++ internal/gameServer/udp.go
@@ -87,10 +87,8 @@ func (g *GameServer) sendUDPInput(count uint32, addr *net.UDPAddr, playerNumber 
 		// Allow a +/-1 difference between the buffer health and the target
 		if bufferHealth > g.BufferTarget+1 {
 			g.GameData.BufferSize -= min(bufferHealth-g.BufferTarget-1, g.GameData.BufferSize)
-		} else if bufferHealth < g.BufferTarget-1 {
+		} else if bufferHealth < g.BufferTarget-1 || bufferHealth == 0 {
 			g.GameData.BufferSize += g.BufferTarget - bufferHealth - 1
-		} else if bufferHealth == 0 {
-			g.GameData.BufferSize += g.BufferTarget
 		}
 	}
 	if sendingPlayerNumber == NoRegID { // if the incoming packet was KeyInfoClient, the regID isn't included in the packet
